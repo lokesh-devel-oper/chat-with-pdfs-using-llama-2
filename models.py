affaf3abf82154c8994347c4d6743e4847c4777b
lokesh-devel-oper <ganeshlokesh01@gmail.com>
2024-01-02 22:01:33 +0530

    Demo Review

--- models.py
+++ models.py
@@ -40,7 +40,7 @@ def retrieval_qa_chain(llm, prompt, db):
 def load_llm():
     # Load the locally downloaded model here
     llm = CTransformers(
-        model = "C:/Users/ganes/LLMs_Development/models/llama-2-7b-chat.ggmlv3.q8_0.bin",
+        model = "../models/llama-2-7b-chat.ggmlv3.q8_0.bin",
         model_type="llama",
         max_new_tokens = 512,
         temperature = 0.5
